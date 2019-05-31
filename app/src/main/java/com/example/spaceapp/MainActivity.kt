0e3e5ed69c7fe029bd095bca5e543ea6561f4933
Arina <doldrums.317@gmail.com>
2019-05-31 23:39:57 +0200

    add main activity xml

--- app/src/main/java/com/example/spaceapp/MainActivity.kt
+++ app/src/main/java/com/example/spaceapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.spaceapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val actionBar = supportActionBar
+        actionBar!!.hide()
+
+        btn_anonymous.setOnClickListener {
+
+        }
     }
 }
