ed6159c23e0ecfc80a8c6462f7e4c98bd0be70cc
Arina <doldrums.317@gmail.com>
2019-06-03 01:33:35 +0200

    lol

--- app/src/main/java/com/example/spaceapp/InfoActivity.kt
+++ app/src/main/java/com/example/spaceapp/InfoActivity.kt
@@ -2,9 +2,12 @@ package com.example.spaceapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_info.*
+import com.jjoe64.graphview.series.LineGraphSeries
+import com.jjoe64.graphview.series.DataPoint
+import kotlinx.android.synthetic.main.content_info.*
+
 
 class InfoActivity : AppCompatActivity() {
 
@@ -12,9 +15,27 @@ class InfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
         setSupportActionBar(toolbar)
-        fab.setOnClickListener { view ->
+        fab.setOnClickListener {
             intent = Intent(this,TimeActivity::class.java)
             startActivity(intent)
         }
+
+        val series = LineGraphSeries<DataPoint>(
+            arrayOf<DataPoint>(
+                DataPoint(0.0, 0.0),
+                DataPoint(1.18, 0.19),
+                DataPoint(1.48, 0.32),
+                DataPoint(1.77, 0.4),
+                DataPoint(2.07, 0.6),
+                DataPoint(2.96, 0.67),
+                DataPoint(5.93, 0.7),
+                DataPoint(8.89, 0.74)
+            )
+        )
+        graph.addSeries(series)
+        graph.viewport.isScalable = true
+        graph.viewport.isScrollable = true
+        graph.viewport.setScalableY(true)
+        graph.viewport.setScrollableY(true)
     }
 }
--- app/src/main/java/com/example/spaceapp/TimeActivity.kt
+++ app/src/main/java/com/example/spaceapp/TimeActivity.kt
@@ -3,30 +3,76 @@ package com.example.spaceapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.IntegerRes
 import android.support.design.widget.BottomSheetBehavior
 import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_time.*
 import android.view.MotionEvent
+import android.widget.SeekBar
+import android.widget.TextView
 import kotlinx.android.synthetic.main.bottom_sheet.*
 import kotlinx.android.synthetic.main.content_time.*
 import java.util.*
+import com.jaygoo.widget.RangeSeekBar
+import com.jaygoo.widget.OnRangeChangedListener
+
+
 
 
 class TimeActivity : AppCompatActivity() {
-
-    val r = 1 * 0.2966 * Math.pow(10.0,12.0)
-    val G = 6.67408 *Math.pow(10.0,-11.0)
-    val M = 1.9 * Math.pow(10.0,38.0)
-    val c = 299792458
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time)
 
         var dX  = 100.0F
         var dY = 600.0F
+        var resultR = 0.0
+        var resultM = 0.0
+
+
+        val G = 6.67408*(Math.pow(10.0,-11.0))
+        val c = 299792458
+        var sun =  1.9985*Math.pow(10.0, 30.0)
+        var r = 0.2966*Math.pow(10.0,12.0)
+
+        seek_result_r.setOnRangeChangedListener(object : OnRangeChangedListener {
+            override fun onRangeChanged(view: RangeSeekBar, leftValue: Float, rightValue: Float, isFromUser: Boolean) {
+                //leftValue is left seekbar value, rightValue is right seekbar value
+                txt_result_r.text = leftValue.toDouble().toInt().toString()
+                resultR = leftValue.toDouble()
+
+            }
+
+            override fun onStartTrackingTouch(view: RangeSeekBar, isLeft: Boolean) {
+                //start tracking touch
+            }
+
+            override fun onStopTrackingTouch(view: RangeSeekBar, isLeft: Boolean) {
+                txt_result_r.text = resultR.toInt().toString()
+                txt_result.text = String.format("%.5g%n", Math.sqrt(1-((2*G*resultM*sun)/resultR*r*c*c)))
+            }
+        })
+
+        seek_result_m.setOnRangeChangedListener(object : OnRangeChangedListener {
+            override fun onRangeChanged(view: RangeSeekBar, leftValue: Float, rightValue: Float, isFromUser: Boolean) {
+                //leftValue is left seekbar value, rightValue is right seekbar value
+                txt_result_m.text = leftValue.toDouble().toInt().toString()
+                resultM = leftValue.toDouble()
+
+            }
+
+            override fun onStartTrackingTouch(view: RangeSeekBar, isLeft: Boolean) {
+                //start tracking touch
+            }
+
+            override fun onStopTrackingTouch(view: RangeSeekBar, isLeft: Boolean) {
+                txt_result_m.text = resultM.toInt().toString()
+                txt_result.text = String.format("%.5g%n", Math.sqrt(1-((2*G*resultM*sun)/resultR*r*c*c)))
+            }
+        })
+
 
         val currentTime = Calendar.getInstance().time
         txt_date.text= currentTime.toString()
@@ -125,6 +171,8 @@ class TimeActivity : AppCompatActivity() {
             intent = Intent(this,InfoActivity::class.java)
             startActivity(intent)
         }
+
+
 
     }
 }
